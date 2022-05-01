66db1ddf4a748c3acafadfba54bf1e091200c096
Артем Мартынов <a.martynof2013@yandex.ru>
2022-05-02 00:23:19 +0300

    JavaHW21

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int costTicket = 1000;
+        int rublesForMiles = 20;
+
+        int miles = costTicket / rublesForMiles;
+
+        System.out.println("Заработано миль: " + miles);
+
+    }
+}
